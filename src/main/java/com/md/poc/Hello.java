eab7fba709cac5e48b108fb68e4d5beb4c2f59ea
Divya <divyashree057@gmail.com>
2023-02-13 22:05:40 +0530

    Revert "dsdsd"
    This reverts commit 7bdcfe117ea1800e221e44220da0edc3fd6a850a.

--- src/main/java/com/md/poc/Hello.java
+++ src/main/java/com/md/poc/Hello.java
@@ -6,21 +6,18 @@ public class Hello {
 
 
         int a = 10, b = 20;
-        int e = a + b + 100;
+        int c = a + b + 100;
         int c = a + b + 50;
         System.out.println("output" + c);
          System.out.println("output thanks/...." );
         System.out.println("output thanks/...." );
-        int d=20+a;
          m2();
-
 
         System.out.println("output thanks/...." );
     }
 
     public void m2()
     {
-        System.out.println("output thanks for vist/...." );
 
        System.out.println("output thanks/...." );
     }
